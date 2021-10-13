1aa656944787a2b7ce74ec3583120caea8431458
Maksim Basov <maksimbasov1992@mail.ru>
2021-10-13 18:17:57 +0300

    feat: js progress-container

--- index.js
+++ index.js
@@ -79,4 +79,46 @@ const authFormValidator = new FormValidation(authForm)
 regFormValidator.enableValidation();
 authFormValidator.enableValidation();
 
-const progress
+const progress = document.getElementById("progress");
+const prev = document.getElementById("btnPrev");
+const next = document.getElementById("btnNext");
+const circles = document.querySelectorAll(".circle");
+
+let currentValue = 1;
+
+next.addEventListener("click", () => {
+    currentValue++;
+   if (currentValue > circles.length) {
+       currentValue = circles.length;
+   }
+   update();
+});
+
+prev.addEventListener("click", () => {
+    currentValue--;
+   if (currentValue < 1) {
+       currentValue = 1;
+   }
+   update();
+});
+
+function update() {
+    circles.forEach((circle, index) => {
+        if (index < currentValue) {
+            circle.classList.add("circle__type_active");
+        } else {
+            circle.classList.remove("circle__type_active");
+        }
+    })
+}
+
+const actives = document.querySelectorAll(".circle__type_active");
+progress.style.width = ((actives.length -1) / (circles.length - 1)) * 100 + '%';
+if (currentValue === circles.length) {
+    next.disabled = true;
+} else if (currentValue <= 1) {
+    prev.disabled = true;
+} else {
+    next.disabled = false;
+    prev.disabled = false;
+}
