abb5f42621e72d8339d1dc3a5654a15a5b1a0207
Maksim Basov <maksimbasov1992@mail.ru>
2021-10-15 15:16:13 +0300

    fix: progress fixed

--- index.js
+++ index.js
@@ -14,54 +14,15 @@ const authFormValidator = new FormValidation(authForm)
 regFormValidator.enableValidation();
 authFormValidator.enableValidation();
 
-console.log(circles)
-
-let currentValue = 1;
-
 regForm.addEventListener("submit", () => {
     const circleFirst = document.querySelector('.circle-first');
-    const circleSecond = document.querySelector('.circle-second')
+    const circleSecond = document.querySelector('.circle-second');
     circleFirst.classList.add("circle__type_done");
     circleSecond.classList.add("circle__type_active");
+    progress.style.width = '100%';
 });
 
 authForm.addEventListener("submit", () => {
-    const circle = document.querySelector('.circle-second')
-    circle.classList.add("circle__type_active");
+    const circleSecond = document.querySelector('.circle-second')
+    circleSecond.classList.add("circle__type_done");
 });
-
-prev.addEventListener("click", function () {
-    currentValue--;
-    if (currentValue < 1) {
-        currentValue = 1;
-    }
-    update();
-});
-
-function update() {
-    circles.forEach((circle, index) => {
-        if (index < currentValue) {
-            circle.classList.add("circle__type_active");
-        } else {
-            circle.classList.remove("circle__type_active");
-        }
-    });
-
-
-    const actives = document.querySelectorAll(".circle__type_active");
-    progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
-    if (currentValue === circles.length) {
-        next.disabled = true;
-    } else if (currentValue <= 1) {
-        prev.disabled = true;
-    } else {
-        next.disabled = false;
-        prev.disabled = false;
-    }
-}
-
-
-
-
-
-
